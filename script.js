22790c24a47f55046424f2bf9aeccffada59befa
insanity11 <83481569+insanity11@users.noreply.github.com>
2022-07-26 23:37:21 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,66 @@
-let v = "2343 + 2430"
-console.log(Function(v))
+const one = document.getElementById('one')
+const two = document.getElementById('two')
+const three = document.getElementById('three')
+const four = document.getElementById('four')
+const five = document.getElementById('five')
+const six = document.getElementById('six')
+const seven = document.getElementById('seven')
+const eight = document.getElementById('eight')
+const nine = document.getElementById('nine')
+const plus = document.getElementById('plus')
+const minus= document.getElementById('minus')
+const divide = document.getElementById('divide')
+const multply = document.getElementById('multply')
+const clear = document.getElementById('clear')
+const AClear = document.getElementById('AClear')
+
+const input = document.getElementById("inputField")
+
+
+one.addEventListener("click",function(){
+  console.log("one")
+})
+two.addEventListener("click",function(){
+  console.log("two")
+})
+three.addEventListener("click",function(){
+  console.log("three")
+})
+four.addEventListener("click",function(){
+  console.log("four")
+})
+five.addEventListener("click",function(){
+  console.log("five")
+})
+six.addEventListener("click",function(){
+  console.log("six")
+})
+seven.addEventListener("click",function(){
+  console.log("seven")
+})
+eight.addEventListener("click",function(){
+  console.log("eight")
+})
+nine.addEventListener("click",function(){
+  console.log("nine")
+})
+plus.addEventListener("click",function(){
+  console.log("plus")
+})
+minus.addEventListener("click",function(){
+  console.log("minus")
+})
+divide.addEventListener("click",function(){
+  console.log("divide")
+})
+multply.addEventListener("click",function(){
+  console.log("multply")
+})
+clear.addEventListener("click",function(){
+  console.log("clear")
+})
+AClear.addEventListener("click",function(){
+  console.log("AClear")
+})
+
+
